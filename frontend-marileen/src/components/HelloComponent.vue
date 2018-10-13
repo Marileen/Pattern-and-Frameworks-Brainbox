2f742843f1d4633b3349b146574fc3ba3de75a74
Marileen Stamer <marileen@gmx.net>
2018-10-13 13:27:26 +0200

    Vue : erste Compontent zum Laufen bekommen gitignore erweitert gradle stuff eingecheckt

---
+++ frontend-marileen/src/components/HelloComponent.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div>
+        <h1>Hello {{ name }} - {{ age }}!</h1>
+        <button v-on:click="incAge()"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        data : () => {
+            return {
+                age : 56
+            }
+        },
+        methods : {
+            incAge() {
+                this.age+=1;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: red;
+    }
+</style>
--- frontend-marileen/webpack.config.js
+++ frontend-marileen/webpack.config.js
@@ -1,21 +1,49 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
 
-    entry : './js-webpack/script1.js',
-    output : {
-        path : __dirname + '/dist',
-        filename: 'bundle.js'
+    //entry : './js-webpack/script1.js',
+
+    // output : {
+    //     path : __dirname + '/dist',
+    //     filename: 'bundle.js'
+    // },
+
+    entry : './src/app.js',
+
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
     },
 
     module: {
         rules: [
             {
-                test: /\.scss/,
+                test: /\.scss$/,
                 use: [
-                    "style-loader", // creates style nodes from JS strings
+                    "vue-style-loader", // creates style nodes from JS strings
                     "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
                 ]
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
             }
-        ]
-    }
+        ] //rules
+    }, // module
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
 };
